4d1e407ce66a4f11f9c10395f99a9ff1e40361e1
Manahil Aamir <112062135+Manahil-Aamir@users.noreply.github.com>
2024-02-13 00:01:09 +0500

    calculation logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -23,12 +26,135 @@ class Calculator extends StatefulWidget{
 
 class _CalculatorState extends State<Calculator>{
 
+//variables
+dynamic value = '0';
+String numberone = '0';
+String numbertwo = '0';
+dynamic displayresult = 0;
+dynamic finalresult = 0;
+dynamic operation = '';
+dynamic prevoperation = '';
+bool val = false;
+
+//calculator basic calculations logic
+void calculations(buttontext){
+
+  if(buttontext == 'AC'){
+    numberone = '0';
+    numbertwo = '0';
+    displayresult = 0;
+    finalresult = 0;
+    val = false;
+  }
+
+  else if(buttontext == '+/-')
+{
+  _signchange();
+}
+
+ else if(buttontext == '+' || buttontext == '-' || buttontext == '×' || buttontext == '÷' || buttontext == '%' || buttontext == '=' ){
+  prevoperation = operation;
+  operation = buttontext;
+  val = true;
+  if(prevoperation == '+'){
+      _add();
+    }
+    else if (prevoperation == '-'){
+      _subtract();
+  }
+  else if(prevoperation == '×'){
+    _multiply();
+  }
+    else if(prevoperation == '÷'){
+    _divide();
+  }
+    else if(prevoperation == '%'){
+    _percent();
+  }
+
+}
+
+else{
+ if (!val) {
+    numberone = numberone.startsWith('0') ? buttontext : numberone + buttontext;
+    finalresult = numberone;
+  } else {
+    numbertwo = numbertwo.startsWith('0') ? buttontext : numbertwo + buttontext;
+    finalresult = numbertwo;
+  }
+}
+_format(finalresult.toString());
+setState(() {
+  finalresult = finalresult;
+  displayresult = displayresult;
+});
+}
+
+void _add(){
+  finalresult = _parseNumber(numberone) + _parseNumber(numbertwo);
+  numberone = finalresult.toString();
+  numbertwo = '0';
+}
+
+void _subtract(){
+  finalresult = _parseNumber(numberone) - _parseNumber(numbertwo);
+  numberone = finalresult.toString();
+  numbertwo = '0';
+}
+
+void _multiply(){
+  finalresult = _parseNumber(numberone) * _parseNumber(numbertwo);
+  numberone = finalresult.toString();
+  numbertwo = '0';
+}
+
+void _divide(){
+  finalresult = _parseNumber(numberone) / _parseNumber(numbertwo);
+  numberone = finalresult.toString();
+  numbertwo = '0';
+}
+
+void _percent(){
+  finalresult = finalresult * 0.01;
+  numberone = finalresult.toString();
+}
+
+void _signchange(){
+  finalresult = finalresult*-1;
+  numberone = finalresult.toString();
+}
+
+num _parseNumber(String number) {
+  if (number.contains('.')) {
+    return double.parse(number);
+  } else {
+    return int.parse(number);
+  }
+}
+
+void _format(String number) {
+  num numValue = num.parse(number);
+  String str;
+  if (numValue is int) {
+    str = numValue.toString();
+    str = str.length > 9 ? str.substring(0, 9) : str;
+  } else {
+    str = numValue.toStringAsFixed(9);
+    List<String> parts = str.split('.');
+    parts[0] = parts[0].length > 9 ? parts[0].substring(0, 9) : parts[0];
+    parts[1] = parts[1].length > 9 ? parts[1].substring(0, 9) : parts[1];
+    str = parts.join('.');
+  }
+  final formatter = NumberFormat("#,###.#########", "en_US");
+  displayresult = formatter.format(double.parse(str));
+}
+
 //It will return the particular button whose context is passed
 Widget buttons(String buttontext, Color buttoncolor, Color textColor){
   return Container(
     child: ElevatedButton(
       onPressed: (){
-
+          calculations(buttontext);
       },
       style: ButtonStyle(
         shape: MaterialStateProperty.all(CircleBorder()),
@@ -56,7 +182,7 @@ Widget zerobutton(String buttontext, Color buttoncolor, Color textColor){
   return Container(
     child: ElevatedButton(
       onPressed: (){
-
+          calculations(buttontext);
       },
       style: ButtonStyle(
         shape: MaterialStateProperty.all(StadiumBorder()),
@@ -93,17 +219,24 @@ Widget zerobutton(String buttontext, Color buttoncolor, Color textColor){
           children: [
 
             //creating the UI for the final answer that will be displayed
-            Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(30.0),
-                    child: Text('0', textAlign: TextAlign.left,  style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 100
-                    )
-                    )
-                    )
-                ],
-              ),
+          Row(
+  mainAxisAlignment: MainAxisAlignment.end,
+  children: [
+    Expanded(
+      child: Padding(
+        padding: EdgeInsets.all(30.0),
+        child: AutoSizeText(
+          '$displayresult',
+          textAlign: TextAlign.right,
+          style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 90),
+          maxLines: 1,
+        ),
+      ),
+    ),
+  ],
+),
+
+
 
               Row(
                 
